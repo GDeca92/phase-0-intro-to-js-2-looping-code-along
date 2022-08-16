7933c88bbcd6fb5cbb31c6c8d8ad7861408feafa
Gian Carlo De Castro <Giancld92@gmail.com>
2022-08-16 16:57:41 -0500

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// Code your solutions in this file
+const name = ["Guadalupe", "Ollie", "Aki"];
+const event = ["surprise"]
+
+function writeCards(name, event) {
+    let thankYouCards = []
+    for (let i = 0; i < name.length; i++) {
+        thankYouCards.push(`Thank you, ${name[i]}, for the wonderful ${event} gift!`);
+    }
+    return thankYouCards;
+}
+
+const start = (10)
+function countDown(start) {
+    while (start > 0) {
+        console.log(start--);
+    }
+    console.log(start)
+}
